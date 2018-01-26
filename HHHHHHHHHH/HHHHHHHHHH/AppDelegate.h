74b3be1e71651feea714ad06d1b9618f6f76d4e1
hey1207 <hey_iosdeveloper@163.com>
2018-01-26 10:41:10 +0800

    3333

---
+++ HHHHHHHHHH/HHHHHHHHHH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHHHHHHHHH
+//
+//  Created by HY on 2018/1/26.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
